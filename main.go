d5fe098d02e08bec447c47e0982a5ce34e372fd9
Jeppe Roi Kristensen <jeppe@jeppek.dk>
2017-09-19 00:16:38 +0200

    Added inital analysis of data

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
+import (
+	"log"
+
+	"github.com/davecgh/go-spew/spew"
+)
+
 func main() {
 	job := createJobFromFlags()
 
-	job.Hey()
+	result, err := job.Analyze()
+	spew.Dump(result)
+	checkError(err)
+
+}
+
+// checkError checks if the pased in err is nil. If not it will
+// it will fail
+func checkError(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
 }
--- sqljob.go
+++ sqljob.go
@@ -3,13 +3,16 @@ package main
 import (
 	"database/sql"
 	"flag"
+	"fmt"
 	"log"
+	"reflect"
+	"strconv"
 
 	_ "github.com/lib/pq"
 )
 
 type analyzer struct {
-	conn *connection
+	wconn *connection
 }
 
 type connection struct {
@@ -37,8 +40,110 @@ type SqlJob struct {
 	connection *connection
 }
 
-func (s SqlJob) Hey() {
-	return
+type ResultMap struct {
+	columnName   string
+	kind         reflect.Kind
+	exampleValue interface{}
+}
+
+// Analyze uses data from the SqlJob to call the query and
+// returns an AnalysisResult
+func (s SqlJob) Analyze() ([]ResultMap, error) {
+
+	rows, err := s.connection.db.Query(s.query)
+	if err != nil {
+		return nil, err
+	}
+
+	columns, _ := rows.Columns()
+	count := len(columns)
+	res := make([]interface{}, count)
+	valuePtrs := make([]interface{}, count)
+
+	for i, _ := range columns {
+
+		fmt.Println(i)
+		valuePtrs[i] = &res[i]
+	}
+
+	rows.Next()
+	fmt.Println(len(valuePtrs))
+	err = rows.Scan(valuePtrs...)
+	if err != nil {
+		return nil, err
+	}
+
+	result := make([]ResultMap, count)
+
+	for i, _ := range columns {
+		result[i] = ResultMap{columns[i], reflect.TypeOf(res[i]).Kind(), res[i]}
+	}
+
+	return result, nil
+
+}
+
+func Dump(i interface{}) {
+	r := reflect.TypeOf(i)
+	val := reflect.ValueOf(i).Elem()
+	//v := reflect.ValueOf(r)
+
+	for i := 0; i < val.NumField(); i++ {
+		valueField := val.Field(i)
+		typeField := val.Type().Field(i)
+		//tag := typeField.Tag
+
+		fmt.Printf("%s  %v\n", typeField.Name, GetValueOrDefault(valueField))
+	}
+
+	fmt.Println(r.NumMethod())
+
+	for i := 0; i < r.NumMethod(); i++ {
+		valueField := r.Method(i)
+		nm := valueField.Name
+		fmt.Println(valueField.Type.Name())
+		fmt.Println(nm)
+
+	} // for index := 0; index < fields; index++ {
+	// 	field := v.Type().Field(index)
+	//alueAsString := GetValueAsString(valueField)
+
+	// 	fmt.Printf("%v %v %v \n", field.Name, field.Type.Kind(), valueAsString)
+	// }
+}
+
+func GetValueOrDefault(value reflect.Value) string {
+	if !value.CanInterface() {
+		return "[UNK]"
+	}
+
+	return fmt.Sprintf("%v", value.Interface())
+}
+
+func GetValueAsString(v reflect.Value) string {
+	var valueInterface interface{}
+
+	if !v.CanInterface() {
+		valueInterface = v
+	}
+
+	valueInterface = v.Interface()
+	val := reflect.ValueOf(valueInterface)
+
+	switch val.Kind() {
+	case reflect.Int, reflect.Int16, reflect.Int32, reflect.Int64, reflect.Int8:
+		return strconv.FormatInt(val.Int(), 10)
+	case reflect.String:
+		return val.String()
+	case reflect.Bool:
+		return strconv.FormatBool(val.Bool())
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+		return strconv.FormatUint(val.Uint(), 10)
+	case reflect.Float32, reflect.Float64:
+		return "Unkown right now"
+	default:
+		return fmt.Sprintf("<%v>", val.Kind())
+	}
 }
 
 func NewJob(query string, connectionstring string, database string) SqlJob {
